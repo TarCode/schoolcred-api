1c90f17665cf91349b2cc9b4c2659551b9cbf102
TarCode <tarcode33@gmail.com>
2017-12-22 08:01:36 +0200

    added morgan logger, item routes and db options

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = express();
+const express = reuiqre('express');
+const app = express();
+const mongoose = reuqire('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const port = 8080;
 
-const port = 8000;
+const item = require('./routes/item');
+let config = require('config');
+
+let options = {
+	server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+	replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};
+
+//db connection      
+mongoose.connect(config.DBHost, options);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+	app.use(morgan('combined'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.get('/', (req, res) => {
+	res.json({ message: "WElcome to the API!" })
+})
+
+app.route('/item')
+	 .get(item.getItems)
+	 .post(item.postItem);
+
+app.route('/item:id')
+	 .get(item.getItem)
+	 .delete(item.deleteItem)
+	 .put(item.updateItem);
 
 app.listen(port, () => {
-	console.log('We are live on ' + port);
-});
+	console.log("Listening on port ", port);
+})
+
+module.exports = app;
